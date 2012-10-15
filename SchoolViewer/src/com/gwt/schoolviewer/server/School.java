62d7a5062ebbec32e425e673fe9f5f217d2cf353
Matthew Dorfmann <whytookay@gmail.com>
2012-10-15 14:47:00 -0700

    Adding school class on the server

---
+++ SchoolViewer/src/com/gwt/schoolviewer/server/School.java
@@ -0,0 +1,13 @@
+package com.gwt.schoolviewer.server;
+
+public class School {
+
+	String name;
+	String location;
+	String district;
+	
+	public School() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
