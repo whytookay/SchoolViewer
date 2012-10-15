9b3fed571fbce05f1056094f4897c800d769c3dc
William Hsiao <willy40702@gmail.com>
2012-10-15 16:49:36 -0700

    sever login info

--- SchoolViewer/src/com/gwt/schoolviewer/client/LoginService.java
+++ SchoolViewer/src/com/gwt/schoolviewer/client/LoginService.java
@@ -1,5 +1,9 @@
 package com.gwt.schoolviewer.client;
 
-public interface LoginService {
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
+@RemoteServiceRelativePath("login")
+public interface LoginService extends RemoteService {
+  public LoginInfo login(String requestUri);
 }
