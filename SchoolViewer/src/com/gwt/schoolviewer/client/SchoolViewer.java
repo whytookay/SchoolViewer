5203bf824fcc43c0d5ab3281f60aeb96937fb8f3
William Hsiao <willy40702@gmail.com>
2012-10-15 18:17:38 -0700

    tryin

--- SchoolViewer/src/com/gwt/schoolviewer/client/SchoolViewer.java
+++ SchoolViewer/src/com/gwt/schoolviewer/client/SchoolViewer.java
@@ -69,7 +69,8 @@ public class SchoolViewer implements EntryPoint {
 		    signInLink.setHref(loginInfo.getLoginUrl());
 		    loginPanel.add(loginLabel);
 		    loginPanel.add(signInLink);
-		    RootPanel.get(null).add(loginPanel);
+		    RootPanel.get(null).add(loginPanel); 
+		    
 		  }
 	
 	private void loadschoolviewer(){
