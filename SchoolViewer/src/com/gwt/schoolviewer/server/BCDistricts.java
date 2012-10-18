d6dbaeced6338efc6c12a78c75ab6887cb3b0fd7
Matthew Dorfmann <whytookay@gmail.com>
2012-10-17 20:05:16 -0700

    created the BCDistricts class

---
+++ SchoolViewer/src/com/gwt/schoolviewer/server/BCDistricts.java
@@ -0,0 +1,13 @@
+package com.gwt.schoolviewer.server;
+
+import java.util.ArrayList;
+
+public class BCDistricts {
+
+	ArrayList<District> districts = new ArrayList<District>();
+	
+	public BCDistricts() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
